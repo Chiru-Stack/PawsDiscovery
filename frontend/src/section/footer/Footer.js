fc2cec0863f27d0e55f8a2a6ce5863c247a908cf
kaarthikeyak <kaarthu2003@gmail.com>
2023-11-16 12:10:10 +0530

    Changed the Footer Menu

--- frontend/src/section/footer/Footer.js
+++ frontend/src/section/footer/Footer.js
@@ -2,7 +2,7 @@ import classes from './Footer.module.css';
 
 const Footer = () => {
     return <footer className={classes.footer}>
-        <p>&copy; 2022 Lost&Found</p>
+        <p>&copy; Ethnus MERN Stack - 2023 Pets Reunite Hub - Team 130</p>
     </footer>
 };
 
